ddca7122b58f84601f43cea8ba7b60f92c0310d0
Anders Christensen <anders@medieskolerne.dk>
2024-09-01 14:55:30 +0200

    init

---
+++ www/scripts/index.mjs
@@ -0,0 +1,18 @@
+
+const app = {};
+
+app.init = () => {
+
+//     console.log('MediaCollege - Socket Server Client.');
+
+// let btn = document.querySelector('#btnJohnny');
+
+
+// btn.addEventListener('click', () => {
+//     console.log('Button Clicked');
+//     io.emit('turnOn');
+// });
+
+};
+
+app.init();
